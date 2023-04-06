c65b0b0abaf889582e8468cc27d62e597f363e3a
Gabriel Lemos Rodrigues <glemosro@uwo.ca>
2023-04-06 00:05:14 -0400

    clean branch for deployment

--- Movie_Booking_System/react-frontend/src/App.js
+++ Movie_Booking_System/react-frontend/src/App.js
@@ -1,21 +1,10 @@
-import {BrowserRouter as Router} from "react-router-dom";
-import LoginPage from './Components/LoginPage/LoginPage';
-import MainPages from './Components/MainPages/MainPages';
 import React from 'react';
-import AnimatedRoutes from './Components/Routes/RoutesComponent';
 
 function App() {
 
-    const [showLogin, setShowLogin] = React.useState(true);
-    const [pageToShow, setPageToShow] = React.useState(null); // pageToShow = [typeOfPage, UsernameForNavBar]
-
     return (
     <div className="App">
-        {/* {showLogin ? <LoginPage setShowLogin={setShowLogin} setPageToShow={setPageToShow}/>
-            : <MainPages setShowLogin={setShowLogin} pageToShow={pageToShow}/>} */}
-        <Router>
-            <AnimatedRoutes />
-        </Router>
+        <h1>Hello there</h1>
     </div>
     );
 }
