42d80362e4cfea56200754ea5c64dfa5b331e97b
Gabriel Lemos Rodrigues <glemosro@uwo.ca>
2023-04-06 17:35:12 -0400

    12 principles done

--- Movie_Booking_System/react-frontend/src/Components/DesignPrinciples/DesignPrinciples.js
+++ Movie_Booking_System/react-frontend/src/Components/DesignPrinciples/DesignPrinciples.js
@@ -6,123 +6,115 @@ function DesignPrinciples() {
     const data = [
         {
             message: `
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            oooooooooooooooooooo
+            For the login page we used the feedback design pattern when the user inputs an incorrect login they get immediate feedback on why it did not go through. For correct login input boxes turn green for a second which tells the user that it went through.
             `,
-            title: "Principle 1"
+            title: "Principle 1 - Login (Feedback)",
+            image: process.env.PUBLIC_URL + "/login-1.jpg"
         },
         {
             message: `
-            22222222222222222222
-            22222222222222222222
-            22222222222222222222
-            22222222222222222222
-            22222222222222222222
-            22222222222222222222
-            22222222222222222222
+            Another design pattern we used for login is visibility. We used this design pattern by adding a blue shade that appears on the input box when the user hovers over it with the mouse. This is helpful because it tells the user they can clearly input something in there.
             `,
-            title: "Principle 2"
+            title: "Principle 2 - Login (Visibility)",
+            image: process.env.PUBLIC_URL + "/login-3.jpg"
         },
         {
             message: `
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            22222222222222222222
-            oooooooooooooooooooo
-            oooooooooooooooooooo
+            We used affordance in the login page by adding a big blue button that says login on it. This provides a clear visual cue to the user that clicking the button will initiate the login process.
             `,
-            title: "Principle 3"
+            title: "Principle 3 - Login (Affordance)",
+            image: process.env.PUBLIC_URL + "/login-4.jpg"
         },
         {
             message: `
-            oooooooooooooooooooo
-            22222222222222222222
-            22222222222222222222
-            22222222222222222222
-            22222222222222222222
-            22222222222222222222
-            oooooooooooooooooooo
+            The login page also uses the consistency design pattern because it has the same style and appearance as the internal elements within the system. For example the blue buttons,black border,and white background.
             `,
-            title: "Principle 4"
+            title: "Principle 4 - Login (Consistency)",
+            image: process.env.PUBLIC_URL + "/login-6.jpg"
         },
         {
             message: `
-            22222222222222222222
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            22222222222222222222
+            Another design pattern that was used for the login page is visual representation. This design pattern was used by making the user and password input boxes right on top of eachother which tells the user that these two fields are meant to be written together.
             `,
-            title: "Principle 5"
+            title: "Principle 5 (Representation)",
+            image: process.env.PUBLIC_URL + "/login-5.jpg"
         },
         {
             message: `
-            22222222222222222222
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            22222222222222222222
+            The bookmark symbol used here is a physical metaphor of bookmarks from physical books, 
+            a straightforward mapping from a real life object to an object on the artifact. 
+            This physical metaphor helps users understand the function of the bookmark symbol and how to use it intuitively, 
+            as it leverages their prior experience with physical books. As a result, users can easily infer that clicking on the 
+            bookmark symbol will save their place in the digital content, just as a physical bookmark would do in a book. 
+            This helps to enhance the usability and user experience of the digital artifact, by reducing the cognitive load 
+            required to learn and understand its features.
             `,
-            title: "Principle 6"
+            title: "Principle 6 - Main Page (Physical Metaphor)",
+            image: process.env.PUBLIC_URL + "/Abbas-1.png"
         },
         {
             message: `
-            22222222222222222222
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            22222222222222222222
+            The bookmark symbol used here can be considered an "iconic sign" because it represents the concept of 
+            bookmarking in a visual, easily recognizable way. Even before interacting with it, 
+            the whited out bookmark symbol with a little plus on it provides users with a visual cue that they can use to 
+            save their place in the digital content. This cue is iconic because it resembles the physical object of a bookmark, 
+            which users can easily associate with the action of saving their place in a book.
             `,
-            title: "Principle 7"
+            title: "Principle 7 - Main Page (Iconic Sign)",
+            image: process.env.PUBLIC_URL + "/Abbas-2.png"
         },
         {
             message: `
-            22222222222222222222
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            22222222222222222222
+            The addition of the word "List" to the bookmark symbol in the nav bar provides users with an explicit 
+            label that indicates the function of the symbol. This label serves as a "affordance" in HCI terminology, 
+            as it suggests to users what action they can take with the symbol.
             `,
-            title: "Principle 8"
+            title: "Principle 8 - Main Page (Affordance)",
+            image: process.env.PUBLIC_URL + "/Abbas-3.png"
         },
         {
             message: `
-            22222222222222222222
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            22222222222222222222
+            The consistency of the bookmark symbol across different parts of the digital artefact (i.e., the main page and the nav bar) 
+            helps to create a sense of familiarity and predictability for users. This allows them to transfer their knowledge and 
+            understanding of the symbol from one context to another, which can improve their efficiency and usability of the digital 
+            artefact.
             `,
-            title: "Principle 9"
+            title: "Principle 9 - Main Page (Aesthetic, Internal Consistency)",
+            image: process.env.PUBLIC_URL + "/Abbas-3.png"
         },
         {
             message: `
-            22222222222222222222
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            22222222222222222222
+            If a user were to accidentally click the unbookmark button, 
+            and the movie were to disappear automatically, it could be frustrating and confusing for the user. 
+            They may wonder where the movie went or if they mistakenly deleted it from their bookmark list. 
+            We chose not to remove the movie automatically, to reduce the likelihood of user slips and improve the user 
+            experience by allowing users to easily correct any mistakes they may have made to prevent what is known as a 
+            "user slip”.
             `,
-            title: "Principle 10"
+            title: "Principle 10 - Main Page (Mitigating User Slips)",
+            image: process.env.PUBLIC_URL + "/Abbas-4.png"
+        },
+        {
+            message: `
+            The affordance of the search bar is provided by the textbox itself, 
+            which indicates that users can enter search terms. The light text that says "search movies..." 
+            also provides a hint about the purpose of the textbox, serving as a placeholder or prompt for users to 
+            enter their search terms. Just as a keyboard or a typewriter has a surface where users can input characters 
+            and symbols, the search box provides a digital surface where users can input search terms.
+            `,
+            title: "Principle 11 - Main Page (Affordance)",
+            image: process.env.PUBLIC_URL + "/Abbas-5.png"
+        },
+        {
+            message: `
+            The real-time matching of search terms to relevant movies is an example of the design principle of progressive disclosure, 
+            which can help reduce the user's memory load. As the user types in search terms, the results are updated in real-time, 
+            gradually revealing the matching movies based on the user's input. By revealing the matching movies in real-time, 
+            the user is spared the effort of having to recall the search terms they entered, which can reduce the cognitive load on the 
+            user's working memory.
+            `,
+            title: "Principle 12 - Main Page (Progressive Disclosure)",
+            image: process.env.PUBLIC_URL + "/Abbas-6.png"
         },
         {
             message: `
@@ -134,7 +126,8 @@ function DesignPrinciples() {
             oooooooooooooooooooo
             22222222222222222222
             `,
-            title: "Principle 11"
+            title: "Principle 13",
+            image: process.env.PUBLIC_URL + "/login-1.jpg"
         },
         {
             message: `
@@ -146,7 +139,8 @@ function DesignPrinciples() {
             oooooooooooooooooooo
             22222222222222222222
             `,
-            title: "Principle 12"
+            title: "Principle 14",
+            image: process.env.PUBLIC_URL + "/login-1.jpg"
         },
         {
             message: `
@@ -158,7 +152,8 @@ function DesignPrinciples() {
             oooooooooooooooooooo
             22222222222222222222
             `,
-            title: "Principle 13"
+            title: "Principle 15",
+            image: process.env.PUBLIC_URL + "/login-1.jpg"
         },
         {
             message: `
@@ -170,7 +165,8 @@ function DesignPrinciples() {
             oooooooooooooooooooo
             22222222222222222222
             `,
-            title: "Principle 14"
+            title: "Principle 16",
+            image: process.env.PUBLIC_URL + "/login-1.jpg"
         },
         {
             message: `
@@ -182,7 +178,8 @@ function DesignPrinciples() {
             oooooooooooooooooooo
             22222222222222222222
             `,
-            title: "Principle 15"
+            title: "Principle 17",
+            image: process.env.PUBLIC_URL + "/login-1.jpg"
         },
         {
             message: `
@@ -194,7 +191,8 @@ function DesignPrinciples() {
             oooooooooooooooooooo
             22222222222222222222
             `,
-            title: "Principle 16"
+            title: "Principle 18",
+            image: process.env.PUBLIC_URL + "/login-1.jpg"
         },
         {
             message: `
@@ -206,7 +204,8 @@ function DesignPrinciples() {
             oooooooooooooooooooo
             22222222222222222222
             `,
-            title: "Principle 17"
+            title: "Principle 19",
+            image: process.env.PUBLIC_URL + "/login-1.jpg"
         },
         {
             message: `
@@ -218,31 +217,8 @@ function DesignPrinciples() {
             oooooooooooooooooooo
             22222222222222222222
             `,
-            title: "Principle 18"
-        },
-        {
-            message: `
-            22222222222222222222
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            22222222222222222222
-            `,
-            title: "Principle 19"
-        },
-        {
-            message: `
-            22222222222222222222
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            oooooooooooooooooooo
-            22222222222222222222
-            `,
-            title: "Principle 20"
+            title: "Principle 20",
+            image: process.env.PUBLIC_URL + "/login-1.jpg"
         },
     ]
     return (
@@ -259,7 +235,7 @@ function DesignPrinciples() {
                         <div className="principleContent">
                             <TextBlock message={principle.message} bigText={false}/>
                             <div className="principleImageWrapper">
-                                <img className="imageTest" src="https://norlon.ca/wp-content/uploads/2021/03/engineeringexterior-web.jpg" alt="" />
+                                <img className="imageTest" src={principle.image} alt="" />
                             </div>
                         </div>
                     </div>
